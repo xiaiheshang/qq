62af0bf40c70eeaf183a3bf1e8d7ef774a241c8c
xiaiheshang <32920521+xiaiheshang@users.noreply.github.com>
2018-08-07 06:17:27 +0000

    Update Message.java

--- qqcode/common/Message.java
+++ qqcode/common/Message.java
@@ -1,17 +1,17 @@
-package com.it18zhang.tcp.qq.common;
+package com.tcp.qq.common;
 
 /**
- * 消息的抽象类
+ * 娑堟伅鐨勬娊璞＄被
  */
 public abstract class Message {
 
-	/* 客户端发送给服务器的消息 */
+	/* 瀹㈡埛绔彂閫佺粰鏈嶅姟鍣ㄧ殑娑堟伅 */
 	public final static int CLIENT_TO_SERVER_SINGLE_CHAT= 0 ; 
 	public final static int CLIENT_TO_SERVER_CHATS= 1 ; 
 	public final static int CLIENT_TO_SERVER_REFRESH_FRIENDS= 2 ; 
 	public final static int CLIENT_TO_SERVER_EXIT= 3 ; 
 	
-	/* 服务器发送给客户端的消息 */
+	/* 鏈嶅姟鍣ㄥ彂閫佺粰瀹㈡埛绔殑娑堟伅 */
 	public final static int SERVER_TO_CLIENT_REFRESH_FRIENTS = 11 ; 
 	public final static int SERVER_TO_CLIENT_SINGLE_CHAT = 12 ; 
 	public final static int SERVER_TO_CLIENT_CHATS = 13 ; 
